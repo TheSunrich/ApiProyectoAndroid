e6be60ad2e43df91b2af03345132888212341948
Ricardo Iván Ramirez Bello <rambellricardo@gmail.com>
2024-12-01 20:28:21 -0600

    Init Commit

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import noteRouter from './routes/noteRouter';
+
+const app = express();
+
+dotenv.config();
+
+// Conectar a MongoDB
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+    console.error('MONGO_URI is not defined in the environment variables');
+    process.exit(1);
+}
+
+mongoose.connect(mongoUri)
+    .then(() => console.log('Connected to MongoAtlas'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+const PORT = process.env.PORT || 3000;
+
+// Configurar y usar cors
+app.use(cors({
+    origin: '*' // Reemplaza con el dominio permitido
+}));
+
+
+// Middlewares
+app.use(express.json());
+app.use('/notes', noteRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
